6aa2c847b34a1a2f22d4883221e4cd81c7898871
Steve X <stevexyh@hotmail.com>
2020-07-04 15:28:55 +0800

    chore(global): Update

--- Lab-Project/Lab01_MNIST_digit-recognition/model_training.py
+++ Lab-Project/Lab01_MNIST_digit-recognition/model_training.py
@@ -15,7 +15,7 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
-from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, Dropout
 from keras.models import Sequential
 from keras.utils import to_categorical
 from run_test import show_result
@@ -51,31 +51,72 @@ model.add(Conv2D(64, (3, 3), input_shape=(28, 28, 1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# 卷积层2,激活层,最大值池化层
+# 卷积层 2,激活层,最大值池化层
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# 一维化, 输出
+# 一维化, 64个神经元的全连接神经网络, dropout 0.2, 输出层
 model.add(Flatten())
 model.add(Dense(64))
+model.add(Dropout(0.2))
 
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
 # 编译, 训练, 保存模型
 model.compile(
+    # 多分类交叉熵损失函数
     loss=keras.losses.categorical_crossentropy,
     optimizer=keras.optimizers.Adam(),
     metrics=['accuracy']
 )
-model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=5)
+
+hist = model.fit(
+    train_X,
+    train_Y_one_hot,
+    batch_size=64,
+    epochs=10,
+    shuffle=True,
+    validation_data=(test_X, test_Y_one_hot)
+)
 model.save('model.h5')
 
-# 评估模型
-test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
-print('Test loss', test_loss)
-print('Test accuracy', test_acc)
 
-# 显示预测结果
-show_result(isInput=False)
+def draw_plot(hist):
+    '''绘制acc/loss曲线'''
+    plt.figure()
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.plot(hist.history['accuracy'])
+    plt.plot(hist.history['val_accuracy'])
+    plt.legend(['Train-acc', 'Validation-acc'], loc='right')
+    plt.savefig('accuracy.png')
+
+    plt.figure()
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.legend(['Train-loss', 'Validation-loss'], loc='right')
+    plt.savefig('loss.png')
+    # plt.show()
+
+
+def evaluate_model(model):
+    '''评估模型'''
+    test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
+    print('Test loss', test_loss)
+    print('Test accuracy', test_acc)
+    print(model.summary())
+
+    global hist
+    print(hist.history)
+    draw_plot(hist)
+
+
+if __name__ == "__main__":
+    evaluate_model(model)
+
+    # 显示预测结果
+    show_result(isInput=False)
